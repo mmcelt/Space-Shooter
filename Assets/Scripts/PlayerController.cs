b750bb89d80e971cfaf76b7142d735cadee668fa
Ron Welch <mmcelt@yahoo.com>
2020-06-23 07:52:13 -0600

    6 - Moving the Ship

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _moveSpeed;
+	[SerializeField] Rigidbody2D _theRB;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		_theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * _moveSpeed;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
