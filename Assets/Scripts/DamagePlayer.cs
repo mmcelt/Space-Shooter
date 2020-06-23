ba01b72493aae51e74c3dce85fd4dc9a2e402826
Ron Welch <mmcelt@yahoo.com>
2020-06-23 17:59:45 -0600

    SECTION 5 - HEALTH: 20 - Player Health System

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] int _damageToInflict = 1;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			HealthManager.Instance.DamagePlayer(_damageToInflict);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+	#region Fields
+
+	public static HealthManager Instance;
+
+	[SerializeField] int _currentHealth, _maxHealth;
+	[SerializeField] GameObject _deathEffect;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else if(Instance != this)
+			Destroy(gameObject);
+	}
+
+	void Start() 
+	{
+		_currentHealth = _maxHealth;
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void DamagePlayer(int amount = 1)
+	{
+		_currentHealth -= amount;
+		_currentHealth = Mathf.Max(0, _currentHealth);
+
+		if (_currentHealth == 0)
+		{
+			Instantiate(_deathEffect, transform.position, Quaternion.identity);
+			gameObject.SetActive(false);
+		}
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
