51c971439c6fc635043adb7f374dd720317793b6
Ron Welch <mmcelt@yahoo.com>
2020-06-23 10:39:00 -0600

    SECTION 4 - SHOTS & EXPLOSIONS: 13 - Creating a Laser Shot

---
+++ Assets/Scripts/PlayerShot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShot : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _shotSpeed = 7f;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		transform.position += new Vector3(_shotSpeed * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+
+		}
+
+		Destroy(gameObject);
+
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
