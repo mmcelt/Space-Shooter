a8745e91b83f127382b94753613060acfe856228
Ron Welch <mmcelt@yahoo.com>
2020-06-24 13:10:43 -0600

    22 - Enemy Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
 	#region Fields
 
 	[SerializeField] float _moveSpeed;
+	[SerializeField] Vector2 _startDirection;
 
 	#endregion
 
@@ -19,7 +20,8 @@ public class EnemyController : MonoBehaviour
 
 	void Update()
 	{
-		transform.position -= new Vector3(_moveSpeed * Time.deltaTime, 0f, 0f);
+		//transform.position -= new Vector3(_moveSpeed * Time.deltaTime, 0f, 0f);
+		transform.position += new Vector3(_startDirection.x, _startDirection.y) * _moveSpeed * Time.deltaTime;
 	}
 
 	void OnBecameInvisible()
