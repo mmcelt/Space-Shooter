35eb342df2354bf58563cc01b9f7b45360600937
Ron Welch <mmcelt@yahoo.com>
2020-06-24 13:02:11 -0600

    SECTION 6 - ENEMIES: 21 - Making Enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _moveSpeed;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+
+	void Update()
+	{
+		transform.position -= new Vector3(_moveSpeed * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
