a13c024fb8099ac3a84c18d077bd4cc6e5d47f9d
Ron Welch <mmcelt@yahoo.com>
2020-06-24 13:32:27 -0600

    23 - Better Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,10 @@ public class EnemyController : MonoBehaviour
 
 	[SerializeField] float _moveSpeed;
 	[SerializeField] Vector2 _startDirection;
+	[Header("Change Direction")]
+	[SerializeField] bool _shouldChangeDirection;
+	[SerializeField] float _changeDirectionXPoint;
+	[SerializeField] Vector2 _changedDirection;
 
 	#endregion
 
@@ -21,7 +25,21 @@ public class EnemyController : MonoBehaviour
 	void Update()
 	{
 		//transform.position -= new Vector3(_moveSpeed * Time.deltaTime, 0f, 0f);
-		transform.position += new Vector3(_startDirection.x, _startDirection.y) * _moveSpeed * Time.deltaTime;
+		if (!_shouldChangeDirection)
+		{
+			transform.position += new Vector3(_startDirection.x, _startDirection.y) * _moveSpeed * Time.deltaTime;
+		}
+		else
+		{
+			if (transform.position.x > _changeDirectionXPoint)
+			{
+				transform.position += new Vector3(_startDirection.x, _startDirection.y) * _moveSpeed * Time.deltaTime;
+			}
+			else
+			{
+				transform.position += new Vector3(_changedDirection.x, _changedDirection.y) * _moveSpeed * Time.deltaTime;
+			}
+		}
 	}
 
 	void OnBecameInvisible()
