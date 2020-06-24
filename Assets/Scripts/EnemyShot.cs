adc315436268812cef33bbc3644103a7c305e537
Ron Welch <mmcelt@yahoo.com>
2020-06-24 14:24:19 -0600

    24 - Creating Enemy Shot

---
+++ Assets/Scripts/EnemyShot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _shotSpeed = 7f;
+	[SerializeField] GameObject _impactEffect;
+	[SerializeField] int _damageToInflict = 1;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		transform.position -= new Vector3(_shotSpeed * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Instantiate(_impactEffect, transform.position, Quaternion.identity);
+
+		if (other.CompareTag("Player"))
+		{
+			HealthManager.Instance.DamagePlayer(_damageToInflict);
+		}
+
+		Destroy(gameObject);
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
