c23296669f0ec0e7d1cf8f07c8d64db4a2bcd036
Ron Welch <mmcelt@yahoo.com>
2020-06-27 18:12:23 -0600

    SECTION 7 - ENEMY WAVES & LIVES: 28 - Making Waves

---
+++ Assets/Scripts/EnemyWave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWave : MonoBehaviour
+{	
+	#region Fields
+
+	
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		transform.DetachChildren();
+		Destroy(gameObject);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
